be045c0012f94cd7338837adc6f76ec797c57889
João Pedro Miguel de Moura <joao.moura@opus-software.com.br>
2018-10-25 11:19:59 -0300

    polimento de código

--- src/app/fetch-data/fetch-data.component.ts
+++ src/app/fetch-data/fetch-data.component.ts
@@ -7,9 +7,10 @@ import { HttpClient } from '@angular/common/http';
 })
 export class FetchDataComponent {
   public tasks: Object;
+  public page: number;
 
   constructor(http: HttpClient, @Inject('BASE_URL') baseUrl: string) {
-    var url = "http://localhost:54380/api/tasks";
+    var url = "http://localhost:54380/api/tasks?page=1&size=10";
     var xhr = new XMLHttpRequest();
     var response;
     xhr.open('GET', url, false);
@@ -17,5 +18,30 @@ export class FetchDataComponent {
     response = JSON.parse(xhr.responseText);
     this.tasks = response;
     console.table(this.tasks);
+    this.page = 1;
+  }
+
+  nextPage() {
+    this.page = this.page + 1;
+    var url = "http://localhost:54380/api/tasks?page=" + this.page + "&size=10";
+    var xhr = new XMLHttpRequest();
+    var response;
+    xhr.open('GET', url, false);
+    xhr.send(null);
+    response = JSON.parse(xhr.responseText);
+    this.tasks = response;
+  }
+
+  previousPage() {
+    if (this.page > 1) {
+      this.page = this.page - 1;
+      var url = "http://localhost:54380/api/tasks?page=" + this.page + "&size=10";
+      var xhr = new XMLHttpRequest();
+      var response;
+      xhr.open('GET', url, false);
+      xhr.send(null);
+      response = JSON.parse(xhr.responseText);
+      this.tasks = response;
+    }
   }
 }
--- src/app/home/collection-list.component.ts
+++ src/app/home/collection-list.component.ts
@@ -42,7 +42,6 @@ export class CollectionListComponent implements OnInit {
         this.done.push(task);
     }
 
-    console.table(response);
   }
 
   ngOnInit() {
@@ -56,11 +55,28 @@ export class CollectionListComponent implements OnInit {
       xhr.open('POST', url, false);
       xhr.setRequestHeader("Content-Type", "application/json");
       xhr.send(JSON.stringify(taskToSend));
-      let task = Object.assign({}, this.item);
-      this.backlog.push(task);
-      this.item.description = "";
-      this.item.hours = 0;
-      this.item.id = 0;
+      url = "http://localhost:54380/api/SprintManager?projectId=" + this.injection.activeProject;
+      xhr = new XMLHttpRequest();
+      var response;
+      xhr.open('GET', url, false);
+      xhr.send(null);
+      response = JSON.parse(xhr.responseText);
+
+      this.backlog = [];
+      this.inProgress = [];
+      this.blocked = [];
+      this.done = [];
+      
+      for (let task of response) {
+        if (task.status == 1)
+          this.backlog.push(task);
+        if (task.status == 2)
+          this.inProgress.push(task);
+        if (task.status == 3)
+          this.blocked.push(task);
+        if (task.status == 4)
+          this.done.push(task);
+      }
     }
   }
   remove() {
@@ -99,50 +115,50 @@ export class CollectionListComponent implements OnInit {
   }
 
   shiftToInProgress(index: number) {
-    if(confirm("Are you sure you want to put this task in progress?")) {
-    var url = "http://localhost:54380/api/tasks/" + this.backlog[index].taskId + "?status=2";
-    console.log(url);
-    let t: Task[] = this.backlog.splice(index, 1);
-    this.inProgress.push(t[0]);
-    var xhr = new XMLHttpRequest();
-    xhr.open('PUT', url, false);
-    xhr.send(null);
+    if (confirm("Are you sure you want to put this task in progress?")) {
+      var url = "http://localhost:54380/api/tasks/" + this.backlog[index].taskId + "?status=2";
+      console.log(url);
+      let t: Task[] = this.backlog.splice(index, 1);
+      this.inProgress.push(t[0]);
+      var xhr = new XMLHttpRequest();
+      xhr.open('PUT', url, false);
+      xhr.send(null);
     }
   }
 
   shiftToBlocked(index: number) {
-    if(confirm("Do you want to block this task?")) {
-    var url = "http://localhost:54380/api/tasks/" + this.inProgress[index].taskId + "?status=3";
-    console.log(url);
-    let t: Task[] = this.inProgress.splice(index, 1);
-    this.blocked.push(t[0]);
-    var xhr = new XMLHttpRequest();
-    xhr.open('PUT', url, false);
-    xhr.send(null);
+    if (confirm("Do you want to block this task?")) {
+      var url = "http://localhost:54380/api/tasks/" + this.inProgress[index].taskId + "?status=3";
+      console.log(url);
+      let t: Task[] = this.inProgress.splice(index, 1);
+      this.blocked.push(t[0]);
+      var xhr = new XMLHttpRequest();
+      xhr.open('PUT', url, false);
+      xhr.send(null);
     }
   }
 
   shiftToBacklog(index: number) {
-    if(confirm("Do you want to put this task back into the backlog?")) {
-    var url = "http://localhost:54380/api/tasks/" + this.blocked[index].taskId + "?status=1";
-    console.log(url);
-    let t: Task[] = this.blocked.splice(index, 1);
-    this.backlog.push(t[0]);
-    var xhr = new XMLHttpRequest();
-    xhr.open('PUT', url, false);
-    xhr.send(null);
+    if (confirm("Do you want to put this task back into the backlog?")) {
+      var url = "http://localhost:54380/api/tasks/" + this.blocked[index].taskId + "?status=1";
+      console.log(url);
+      let t: Task[] = this.blocked.splice(index, 1);
+      this.backlog.push(t[0]);
+      var xhr = new XMLHttpRequest();
+      xhr.open('PUT', url, false);
+      xhr.send(null);
     }
   }
 
   shiftToDone(index: number) {
-    if(confirm("Is this task done?")) {
-    var url = "http://localhost:54380/api/tasks/" + this.inProgress[index].taskId + "?status=4";
-    console.log(url);
-    let t: Task[] = this.inProgress.splice(index, 1);
-    this.done.push(t[0]);
-    var xhr = new XMLHttpRequest();
-    xhr.open('PUT', url, false);
-    xhr.send(null);
+    if (confirm("Is this task done?")) {
+      var url = "http://localhost:54380/api/tasks/" + this.inProgress[index].taskId + "?status=4";
+      console.log(url);
+      let t: Task[] = this.inProgress.splice(index, 1);
+      this.done.push(t[0]);
+      var xhr = new XMLHttpRequest();
+      xhr.open('PUT', url, false);
+      xhr.send(null);
     }
     else this.shiftToBlocked(index);
   }
